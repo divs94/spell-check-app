fa20c8311f9bad5078dd2ed682beb6d121e9ed15
divs94 <divyatlng0@gmail.com>
2022-12-14 15:17:35 +0530

    removed exas

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ function App() {
       <header className="App-header">
       <Grammarly clientId="client_B6f6ozg5b1om4ZWWvzDh6b">
         <h2>Improve Your Spell Writing skills with Us</h2>
-      <label htmlFor="title"  >Title</label>
-      <GrammarlyEditorPlugin>
-        <input id="title" style={{padding:"10px"}}/>
-      </GrammarlyEditorPlugin>
+    
       <GrammarlyEditorPlugin>
         <textarea rows={15} cols={80} style={{fontSize:"1.5rem", borderRadius:"25px" , border:"2px solid cyan", marginTop:"25px", padding:"10px", color:"#333"}}></textarea>
       </GrammarlyEditorPlugin>
