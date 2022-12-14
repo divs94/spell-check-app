7724f8378e8b8652d4c33df3de427a1056529dc8
divs94 <divyatlng0@gmail.com>
2022-12-14 15:19:32 +0530

    added placeholder

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import './App.css';
+import "./App.css";
 import { Grammarly, GrammarlyEditorPlugin } from "@grammarly/editor-sdk-react";
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Grammarly clientId="client_B6f6ozg5b1om4ZWWvzDh6b">
-        <h2>Improve Your Spell Writing skills with Us</h2>
-    
-      <GrammarlyEditorPlugin>
-        <textarea rows={15} cols={80} style={{fontSize:"1.5rem", borderRadius:"25px" , border:"2px solid cyan", marginTop:"25px", padding:"10px", color:"#333"}}></textarea>
-      </GrammarlyEditorPlugin>
-    </Grammarly>
+        <Grammarly clientId="client_B6f6ozg5b1om4ZWWvzDh6b">
+          <h2>Improve Your Spell Writing skills with Us</h2>
+
+          <GrammarlyEditorPlugin>
+            <textarea
+              rows={15}
+              cols={80}
+              placeholder="Write something here...!!!"
+              style={{
+                fontSize: "1.5rem",
+                borderRadius: "25px",
+                border: "2px solid cyan",
+                marginTop: "25px",
+                padding: "10px",
+                color: "#333",
+              }}
+            ></textarea>
+          </GrammarlyEditorPlugin>
+        </Grammarly>
       </header>
     </div>
   );
